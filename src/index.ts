2bccbf941eca0868ab8e25315d1bfd249a49aff8
Josh Mu <mu@joshmu.com>
2020-06-17 10:56:11 +1000

    working

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// https://medium.com/javascript-in-plain-english/creating-a-rest-api-with-jwt-authentication-and-role-based-authorization-using-typescript-fbfa3cab22a4
+
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 import * as express from 'express'
